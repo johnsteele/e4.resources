082795c8205e1fcd1fb43b0e29fd2735ca5830b8
Eduard Bartsch <ebartsch>
2010-07-27 10:55:24 +0000

    Bug 316932 - [sfs] Implement ability to dynamically assign federated content providers: switch to API 0.4 version

--- bundles/org.eclipse.core.resources.semantic/src/org/eclipse/core/resources/semantic/ISemanticFileSystem.java
+++ bundles/org.eclipse.core.resources.semantic/src/org/eclipse/core/resources/semantic/ISemanticFileSystem.java
@@ -18,7 +18,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 /**
  * The Semantic File System interface.
  * 
- * @since 4.0
  * @noimplement This interface is not intended to be implemented by clients.
  * @noextend This interface is not intended to be extended by clients.
  */
